5dfb41508959be958086ea8a23260cb0bb9b06ac
ThalaHemanth <viruhemanth@gmail.com>
2016-12-26 16:31:12 +0530

    Added first component and Configured webpack to use components directly

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
+var TodoApp = require('TodoApp');
 
 
 // Loading foundation
@@ -13,5 +13,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-    <h1> Boiler plate</h1>
-document.getElementById('app'));
+    <TodoApp/>,
+    document.getElementById('app')
+);
